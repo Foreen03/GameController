fddfda461eaa3e78124ce36b6ada17fddf16539f
Foreen <yeohhanyi@gmail.com>
2026-05-22 22:06:53 +0800

    feat: remove unused import

--- app/src/main/java/com/hanyi/gamecontroller/ui/screen/ControllerScreen.kt
+++ app/src/main/java/com/hanyi/gamecontroller/ui/screen/ControllerScreen.kt
@@ -5,16 +5,8 @@ import android.graphics.BitmapFactory
 import android.util.Base64
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
@@ -22,13 +14,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -36,8 +25,6 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import coil.compose.AsyncImage
 import com.hanyi.gamecontroller.domain.model.GamepadConfig
 import com.hanyi.gamecontroller.ui.MainViewModel
-import com.hanyi.gamecontroller.ui.icon.LucideEye
-import com.hanyi.gamecontroller.ui.icon.LucideEyeOff
 import kotlinx.coroutines.flow.map
 import java.io.File
 
@@ -144,8 +131,6 @@ fun ControllerScreen(
 
         // System Buttons
         config.layout.systemComponents?.forEach { systemComponent ->
-            val offsetX = systemComponent.position.x * screenWidth.value
-            val offsetY = systemComponent.position.y * screenHeight.value
             SystemButton(
                 systemComponent = systemComponent,
                 theme = config.theme.button,
--- app/src/main/java/com/hanyi/gamecontroller/ui/screen/SystemButton.kt
+++ app/src/main/java/com/hanyi/gamecontroller/ui/screen/SystemButton.kt
@@ -2,7 +2,6 @@ package com.hanyi.gamecontroller.ui.screen
 
 import FeatherCamera
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
